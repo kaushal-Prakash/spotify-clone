3a5dce482d088585b9dc16abca6436224dc1bfc1
Kaushal Prakash <kingkaushal1289@gmail.com>
2025-01-25 15:47:11 +0530

    added custom colors to tailwind config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,19 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'spotify-green': '#1DB954',
+        'spotify-black': '#191414',
+        'spotify-white': '#FFFFFF',
+        'spotify-dark-gray': '#121212',
+        'spotify-light-gray': '#B3B3B3',
+        'spotify-medium-gray': '#535353',
+        'spotify-dark-green': '#1ED760',
+        'spotify-blue': '#509BF5',
+        'spotify-purple': '#9B5DE5',
+        'spotify-pink': '#F15BB5',
+        'spotify-yellow': '#FEE440',
+        'spotify-orange': '#FF924C',
+        'spotify-red': '#EB1E32',
       },
     },
   },
